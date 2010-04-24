10a43225bb851584ed4dbb9fc1d117a736002ead
AndersSahlin <malakeen@gmail.com>
2010-04-24 01:36:25 -0700

    Disabled debug output

--- src/mailaccount.class.js
+++ src/mailaccount.class.js
@@ -81,7 +81,7 @@ function MailAccount(domain) {
 	this.isDefault;
 	
 	// Debug output (if enabled, might cause memory leaks)
-	var verbose = true;
+	var verbose = false;
 	
 	// Without this/that, no internal calls to onUpdate or onError can be made...
 	var that = this;
