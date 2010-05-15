6640fdb4095985c09befdd87ede9e15045bfbceb
AndersSahlin <malakeen@gmail.com>
2010-05-15 03:36:37 -0700

    Added Swedish translation of the Send Page button tooltip

--- src/i18n.js
+++ src/i18n.js
@@ -436,6 +436,7 @@ languages["sv"] = {
     "optionsLinkTitle" : "Gå till inställningssidan",
     "composeLinkTitle" : "Skapa nytt mail",  
     "noUnreadText" : "Inga olästa mail",
+    "sendPageLinkTitle" : "Skicka länk till sidan",
     "oneUnreadText" : "oläst mail", // One unread item: "1 unread mail"
     "severalUnreadText" : "olästa mail", // Several unread items: "3 unread mail"
     "popupText" : "Du har oläst mail!"
